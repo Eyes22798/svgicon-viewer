5044d69548587963ad98eb70e84c9ca6e343c867
helox <eyes22798@gmail.com>
2023-12-21 11:30:37 +0800

    chore: pnpm install

--- bin/index.js
+++ bin/index.js
@@ -19,6 +19,7 @@ const args = yargs
 
 ;(async function() {
   const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', env: process.env, ...opts })
+  await run('pnpm', ['install'])
   await open('http://localhost:3000')
   await run(`node`, ['./.output/server/index.mjs', `--path=${args.path}`])
 })()
